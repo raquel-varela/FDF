a028140dd0c235d8103148b95aabe43e2e63a9cd
Raquel Varela Da Rocha E Sliva <rvarela-@c2r1s6.42porto.com>
2024-03-23 09:56:55 +0000

    act

--- src/handle_errors.c
+++ src/handle_errors.c
@@ -1,41 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   handle_errors.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvarela- <rvarela-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/22 15:34:29 by rvarela-          #+#    #+#             */
-/*   Updated: 2024/03/23 09:48:11 by rvarela-         ###   ########.fr       */
+/*   Created: 2024/03/23 09:44:23 by rvarela-          #+#    #+#             */
+/*   Updated: 2024/03/23 09:47:28 by rvarela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-/*error handling*/
-# include <errno.h>
-# include <math.h>
-
-
-//functions
-
-//Handle errors
-void    error_msg(char *msg);
-
-
-
-
-//read map
-
-//init_mlx
-
-//transform map
-
-//draw map
-
-
-
-#endif
+void    error_msg(char *msg)
+{
+    ft_printf("Error: %s\n", msg);
+    exit (EXIT_FAILURE);
+}
--- src/main.c
+++ src/main.c
@@ -6,17 +6,36 @@
 /*   By: rvarela- <rvarela-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:33:37 by rvarela-          #+#    #+#             */
-/*   Updated: 2024/03/22 16:30:08 by rvarela-         ###   ########.fr       */
+/*   Updated: 2024/03/23 09:52:59 by rvarela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "fdf.h"
 
+
+
+
+
+
+
 int main(int ac, char **av)
 {
-	if (ac > 1)
-    	return (0);
+	int	fd;
+	
+	if (ac != 2)
+    	error_msg("The input file should be: <name>.fdf");
+	fd = open(av[1], O_RDONLY, 0);
+	if (fd == -1)
+		error_msg("Failed to open the file!");
+	else
+		close(fd);
+		
+
+
+
+		
     if (error_check(ac, av) == 0)
+		//Function DRAW MAP
 		//read map
 			//read line
 			//split line
